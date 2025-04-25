0afc49e488570f1b279c87a81953e0d6e96c81c2
Everest Paudel <paudeleverest09@gmail.com>
2025-04-26 00:07:08 +0545

    feat: Implement Assignment, Class, Exam, Lesson, and Subject list pages with table and pagination components

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -4,70 +4,63 @@ import Image from "next/image";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import Link from "next/link";
-import { parentsData, role } from "@/lib/data";
+import {classesData, role, subjectsData } from "@/lib/data";
 
-type Parent = {
+type Class = {
   id: number;
   name: string;
-  email?: string;
-  students:string[]
-  phone: string;
-  address: string;
-
+ capacity:number;
+ grade:number;
+ supervisor:string;
 };
 const columns = [
   {
-    header: "Info",
-    accessor: "info",
+    header: "Class Name",
+    accessor: "name",
   },
   {
-    header: "Student Names",
-    accessor: "students",
+    header: "Capacity",
+    accessor: "capacity",
+    className: "hidden md:table-cell",
+  
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
     className: "hidden md:table-cell",
   },
-
-  
   {
-    header: "Phone",
-    accessor: "phone",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Address",
-    accessor: "address",
-    className: "hidden lg:table-cell",
+    header: "Superviosor",
+    accessor: "supervisor",
+    className: "hidden md:table-cell",
   },
   {
     header: "Actions",
     accessor: "actions",
   },
 ];
-const ParentListPage = () => {
-  const renderRow = (item: Parent) => (
-    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
-      <td className="flex items-center gap-4 p-4">
-       
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-700">{item?.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.students.join(",")}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
+const ClassListPage = () => {
+  const renderRow = (item: Class) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.name}</td>
+     <td className="hidden md:table-cell">{item.capacity}</td>
+     <td className="hidden md:table-cell">{item.grade}</td>
+     <td className="hidden md:table-cell">{item.supervisor}</td>
+     
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/parents/${item.id}`}>
+          <Link href={`/list/class/${item.id}`}>
             <button className="w-7 h-7 flex  items-center justify-center rounded-full bg-lamaSky">
               <Image src="/edit.png" alt="logo" height={16} width={16} />
             </button>
           </Link>
-       
-           
-              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple ">
-                <Image src="/delete.png" alt="logo" height={16} width={16} />
-              </button>
-            
+
+          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple ">
+            <Image src="/delete.png" alt="logo" height={16} width={16} />
+          </button>
         </div>
       </td>
     </tr>
@@ -78,7 +71,7 @@ const ParentListPage = () => {
       {/* Top */}
       <div className="flex items-center justify-between">
         <h1 className=" hidden  md:block text-lg font-semibold ">
-          All Parents
+          All Classes
         </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
@@ -91,15 +84,17 @@ const ParentListPage = () => {
               <Image src="/sort.png" alt="" height={14} width={14} />
             </button>
 
-{ role ==="admin" && (<button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-<Image src="/plus.png" alt="" height={14} width={14} />
-</button>)}
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                <Image src="/plus.png" alt="" height={14} width={14} />
+              </button>
+            )}
           </div>
         </div>
       </div>
       {/* List */}
 
-      <Table columns={columns} renderRow ={renderRow} data={parentsData}/>
+      <Table columns={columns} renderRow={renderRow} data={classesData} />
       {/* Pagination */}
 
       <Pagination />
@@ -107,4 +102,5 @@ const ParentListPage = () => {
   );
 };
 
-export default ParentListPage;
+export default ClassListPage;
+
